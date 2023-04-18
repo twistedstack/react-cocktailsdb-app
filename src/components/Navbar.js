ad685a69243bfa076dbf3524ffd222a4c2d14de5
twistedstack <kellylynnrock@gmail.com>
2023-04-17 21:58:01 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,6 @@ const Navbar = () => {
     <nav className="navbar bg-body-tertiary">
     <div className="container-xl">
       <Link className="logo" to="/">CocktailsDB</Link>
-      <div className="d-inline-flex p-2 menu">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled">Disabled</a>
-      </div>
     </div>
   </nav>
   );
